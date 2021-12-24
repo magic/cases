1ecb5cea8cfe67b19365ff4c2e982c72b0f89237
jascha <j@jaeh.at>
2021-12-24 17:12:59 +0100

    test: use @magic/test.version

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,12 +1,13 @@
-import { is } from '@magic/test'
+import { version } from '@magic/test'
 import cases from '../src/index.mjs'
 
-export default [
-  { fn: () => cases, expect: is.object, info: 'cases is an object' },
-  { fn: () => cases.camel, expect: is.fn, info: 'cases.camel is a function' },
-  { fn: () => cases.kebab, expect: is.fn, info: 'cases.kebab is a function' },
-  { fn: () => cases.kebabCaps, expect: is.fn, info: 'cases.kebabCaps is a function' },
-  { fn: () => cases.pascal, expect: is.fn, info: 'cases.pascal is a function' },
-  { fn: () => cases.snake, expect: is.fn, info: 'cases.snake is a function' },
-  { fn: () => cases.snakeCaps, expect: is.fn, info: 'cases.snakeCaps is a function' },
-]
+const spec = {
+  camel: 'fn',
+  kebab: 'fn',
+  kebabCaps: 'fn',
+  pascal: 'fn',
+  snake: 'fn',
+  snakeCaps: 'fn',
+}
+
+export default version(cases, spec)
